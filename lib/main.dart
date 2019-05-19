f912333c7094469055e4dd7a33108a9e16d849ce
Ayush Shekhar <ayush221b@gmail.com>
2019-05-19 11:54:56 +0530

    Added desktop implementation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:wallio/app/wallio.dart';
 
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
+
 void main() {
   debugPaintSizeEnabled = false;
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   runApp(Wallio());
 }
